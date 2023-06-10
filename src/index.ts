d676447e03d71308fbdedaccd08112210eacd718
Glockers <megamaxchanel@gmail.com>
2023-06-10 18:42:00 +0300

    fix: moved the logger to a separate file

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 import express, { Request, Response } from 'express';
 import 'dotenv/config';
 import bodyParser from 'body-parser';
-import log4js from 'log4js';
 import { checkDatabaseConnection } from './config/db.config';
-
-const logger = log4js.getLogger();
-logger.level = process.env.LOG_LEVEL as string;
+import logger from './utils/logger';
 
 const app = express();
 const PORT = process.env.API_PORT || 5000;
