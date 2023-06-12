65860f36afcefd2cdbf0ebe63af31a8e8c57b486
Glockers <megamaxchanel@gmail.com>
2023-06-12 22:38:52 +0300

    refactor: move logic db to service

--- src/db/dal/meetup.dal.ts
+++ src/db/dal/meetup.dal.ts
@@ -1,48 +1,15 @@
-import { IDatabaseResponse } from '../interface/databaseResponse.interface';
-import Meetup, { IMeetupAttributes } from '../models/Meetup';
+import Meetup from '../models/Meetup';
 
-export const createMeetup = async (payload: any): Promise<IDatabaseResponse<IMeetupAttributes>> => {
-  const meetup = await Meetup.create(payload);
-  return { data: meetup, status: 200 };
+export const createMeetup = async (payload: any) => {
+  return Meetup.create(payload);
 };
 
-export const deleteMeetupById = async (meetupId: number): Promise<IDatabaseResponse<string>> => {
-  try {
-    const meetup = await Meetup.findByPk(meetupId);
-    if (meetup) {
-      await meetup.destroy();
-      return { data: 'meetup удален!', status: 200 };
-    } else {
-      return { data: 'meetup не найден', status: 404 };
-    }
-  } catch (error) {
-    console.error('Ошибка при удалении meetup:', error);
-    return { data: 'meetup не найден', status: 500 };
-  }
+export const deleteMeetupById = async (meetupId: number) => {
+  return Meetup.findByPk(meetupId);
 };
 
-export const getMeetup = async (meetupId: number): Promise<IDatabaseResponse<IMeetupAttributes>> => {
-  try {
-    const meetup = await Meetup.findByPk(meetupId);
-
-    if (meetup) {
-      return {
-        status: 200,
-        data: meetup.toJSON()
-      };
-    } else {
-      return {
-        status: 404,
-        data: 'Такой ID не найден'
-      };
-    }
-  } catch (error) {
-    console.error('Ошибка на сервере при поиске meetup', error);
-    return {
-      status: 500,
-      data: 'Ошибка на сервере при поиске meetup'
-    };
-  }
+export const getMeetup = async (meetupId: number) => {
+  return Meetup.findByPk(meetupId);
 };
 
 export const getAllMeetup = async () => {
--- src/db/services/meetup.service.ts
+++ src/db/services/meetup.service.ts
@@ -1,16 +1,52 @@
 import { createMeetup, deleteMeetupById, getAllMeetup, getMeetup } from '../dal/meetup.dal';
 import { IDatabaseResponse } from '../interface/databaseResponse.interface';
+import { IMeetupAttributes } from '../models';
 
-export const create = async (payload: any) => {
-  return createMeetup(payload);
+export const create = async (payload: any): Promise<IDatabaseResponse<IMeetupAttributes>> => {
+  try {
+    const createdMeetup = await createMeetup(payload);
+    return { data: createdMeetup, status: 200 };
+  } catch (e) {
+    return { data: 'Error with create new meetup', status: 500 };
+  }
 };
 
-export const deleteById = async (id: number) => {
-  return deleteMeetupById(id);
+export const deleteById = async (id: number): Promise<IDatabaseResponse<string>> => {
+  try {
+    const selecteMeetup = await deleteMeetupById(id);
+    if (selecteMeetup) {
+      await selecteMeetup.destroy();
+      return { data: 'meetup удален!', status: 200 };
+    } else {
+      return { data: 'meetup не найден', status: 404 };
+    }
+  } catch (error) {
+    console.error('Ошибка при удалении meetup:', error);
+    return { data: 'meetup не найден', status: 500 };
+  }
 };
 
-export const getOneById = async (id: number) => {
-  return getMeetup(id);
+export const getOneById = async (id: number): Promise<IDatabaseResponse<IMeetupAttributes>> => {
+  try {
+    const meetup = await getMeetup(id);
+    if (meetup) {
+      return {
+        status: 200,
+        data: meetup.toJSON()
+      };
+    } else {
+      return {
+        status: 404,
+        data: 'Такой ID не найден'
+      };
+    }
+  } catch (error) {
+    console.error('Ошибка на сервере при поиске meetup', error);
+    return {
+      status: 500,
+      data: 'Ошибка на сервере при поиске meetup'
+    };
+  }
 };
 
 export const getAll = async (): Promise<IDatabaseResponse<any>> => {
