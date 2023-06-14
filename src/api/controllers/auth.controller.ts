16cff326da03e48508f25ac1cb28b1d8a122c0ab
Glockers <megamaxchanel@gmail.com>
2023-06-14 14:25:32 +0300

    feat: add passport js auth

--- src/api/controllers/auth.controller.ts
+++ src/api/controllers/auth.controller.ts
@@ -1,9 +1,17 @@
-import { Request, Response } from 'express';
+import { Response } from 'express';
+import { TValidatePayload } from '../middleware/validateDTO.middlware';
+import { IUserDTO, TCreateUserDTO } from '../dto/user.dto';
+import { generateTokens } from '../../config/passport.config';
 
-export const signupUser = async (req: Request, res: Response) => {
-  res.send('signup');
+export const signupUser = async (req: TValidatePayload<TCreateUserDTO>, res: Response) => {
+  const validationPayload = req.validatedPayload;
+  res.send(validationPayload);
 };
 
-export const loginUser = async (req: Request, res: Response) => {
-  res.send('login');
+export const loginUser = async (req: TValidatePayload<IUserDTO>, res: Response) => {
+  const validationPayload = req.validatedPayload;
+  console.log(validationPayload);
+  const tokens = generateTokens({ username: 'max' });
+  return res.json(tokens);
+  // res.send(validationPayload);
 };
--- src/api/routes/auth.router.ts
+++ src/api/routes/auth.router.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { loginUser, signupUser } from '../controllers/auth.controller';
+import { validateUserDTO } from '../middleware/auth.moddleware';
 
 const authRouter = Router();
 
-authRouter.post('/login', loginUser);
-authRouter.post('/signup', signupUser);
+authRouter.post('/login', validateUserDTO(), loginUser);
+authRouter.post('/signup', validateUserDTO(), signupUser);
 
 export default authRouter;
